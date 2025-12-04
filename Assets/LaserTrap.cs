2353384964f6c43162c1cf9108dc4430a2e4ae0a
Bashar <basharzin9@gmail.com>
2025-12-04 19:08:40 +0200

    bashar

--- Assets/LaserTrap.cs
+++ Assets/LaserTrap.cs
@@ -2,29 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserTrapController : MonoBehaviour
+public class LaserTrap : MonoBehaviour
 {
+
     public int damage = 1;
+
     private PlayerStats playerStats;
 
+
+
     void Start()
+
     {
+
         playerStats = FindObjectOfType<PlayerStats>();
+
     }
+
+
 
     void OnTriggerEnter2D(Collider2D other)
+
     {
+
         if (other.CompareTag("Player") && playerStats != null)
+
         {
+
             playerStats.TakeDamage(damage);
+
         }
+
     }
 
+
+
     void OnTriggerStay2D(Collider2D other)
+
     {
+
         if (other.CompareTag("Player") && playerStats != null)
+
         {
+
             playerStats.TakeDamage(damage);
+
         }
+
     }
+
 }
---
+++ Assets/LaserTrapController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrapController : MonoBehaviour
+{
+    public int damage = 1;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStats playerStats = other.GetComponent<PlayerStats>();
+
+            if (playerStats != null)
+            {
+                // تأكد إن اسم الحالة هنا هو نفس اسم الحالة في الأنيميتور عندك
+                if (anim.GetCurrentAnimatorStateInfo(0).IsName("active traps"))
+                {
+                    playerStats.TakeDamage(damage);
+                }
+            }
+        }
+    }
+}
